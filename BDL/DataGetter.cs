0e1f7c1ec3d535d58ddb33cbf752fa130d7b2383
einarpe <einarpe@github.com>
2019-04-24 21:06:51 +0200

    Coś jest

---
+++ BDL/DataGetter.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDL
+{
+    public class DataGetter
+    {
+        [SqlFunction(FillRowMethodName = "RandomizerFillRow")]
+        public static IEnumerable Randomizer(int count)
+        {
+            ArrayList resultCollection = new ArrayList();
+            for (int i = 1; i <= count; i++)
+                resultCollection.Add(new RandomizerRow(i));
+
+            return resultCollection;
+        }
+
+        public static void RandomizerFillRow(object obRow, out int Id, out double Value)
+        {
+            var row = obRow as RandomizerRow;
+            Id = row.Id;
+            Value = row.Value;
+        }
+
+        public async static IEnumerable Subjects(int? parentId)
+        {
+            // await Downloader.DownloadXML("").Result;
+            return null;
+        }
+
+        public static IEnumerable Variables(int subjectId)
+        {
+            return null;
+        }
+
+        public static IEnumerable DataByVariable(int variableId)
+        {
+            return null;
+        }
+
+    }
+
+    public class RandomizerRow
+    {
+        private static readonly Random rnd = new Random();
+
+        public int Id;
+        public double Value = rnd.NextDouble();
+
+        public RandomizerRow(int id)
+        {
+            this.Id = id;
+        }
+    }
+}
---
+++ BDL/Downloader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BDL
+{
+    public class Downloader
+    {
+        public static string X_CLIENT_ID = "";
+        
+        public static void SetClientId(string newClientId)
+        {
+            X_CLIENT_ID = newClientId;
+        }
+
+        public static async Task<XDocument> DownloadXML(string requestUri)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-ClientId", X_CLIENT_ID);
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return XDocument.Parse(responseBody);
+            }
+        }
+    }
+}

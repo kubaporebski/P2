c6706210046d1117f4ab3a84b352ad1639766178
Mateusz Bruchnicki <burbongw@gmail.com>
2019-06-13 19:29:40 +0200

    Dodane_Attributes i Measures

---
+++ BDL_DB/staging/Tables/Attributes.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE STAGING.Attributes
+(
+	[Id] [int],
+	[Name] [nvarchar](255),
+	[Symbol] [nvarchar](255) NULL,
+	[Description] [nvarchar](255) NULL,
+CONSTRAINT [PK_ATTRIBUTES] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+))
---
+++ BDL_DB/staging/Tables/Measures.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE STAGING.Measures
+(
+	[Id] [int],
+	[Name] [nvarchar](255),
+	[Description] [nvarchar](255) NULL,
+CONSTRAINT PK_MEASURES PRIMARY KEY CLUSTERED
+(
+ID ASC
+))

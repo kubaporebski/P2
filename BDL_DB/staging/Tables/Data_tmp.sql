2c814afb1f0d43bed5fae1818c929f2b28a5eda4
Mateusz Bruchnicki <burbongw@gmail.com>
2019-06-29 00:00:16 +0200

    test

---
+++ BDL_DB/staging/Tables/Data_tmp.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE staging.Data_tmp (
+    [VariableId] int,
+    [MeasureUnitId] int,
+    [AggregateId] int,
+    [Id] nvarchar(100),
+    [Name] nvarchar(255),
+    [Year] int,
+    [Value] nvarchar(255),
+    [AttributeId] int
+)

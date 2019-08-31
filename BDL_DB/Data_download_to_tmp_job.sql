b29c7f799202fc95793ead93aaa758e0750daff0
Mateusz Bruchnicki <burbongw@gmail.com>
2019-08-31 16:26:34 +0200

    Dodałem skrypt tworzący joba bazodanowego do pobierania danych, ale niestety on zawiera dane dla konkretnego komputera i serwera - proponuję żeby któryś z Was na lapku do odpalił, bo ja bym musiał stacjonarkę na noc zostawić włączoną na kilka dni. Można też dodać uruchomienie całej reszty z pierwszej paczki jako joba.
    Koniecznie zróbcie Deploy dla BDL_Integration - doszła paczka.

---
+++ BDL_DB/Data_download_to_tmp_job.sql
@@ -0,0 +1,61 @@
+﻿USE [msdb]
+GO
+DECLARE @jobId BINARY(16)
+EXEC  msdb.dbo.sp_add_job @job_name=N'Download_data_tmp', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'DESKTOP-0NUGCCL\burbo', @job_id = @jobId OUTPUT
+select @jobId
+GO
+EXEC msdb.dbo.sp_add_jobserver @job_name=N'Download_data_tmp', @server_name = N'DESKTOP-0NUGCCL'
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_add_jobstep @job_name=N'Download_data_tmp', @step_name=N'Download_Data_tmp', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_fail_action=2, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'SSIS', 
+		@command=N'/ISSERVER "\"\SSISDB\BDL\BDL_Integration\Download_data_tmp.dtsx\"" /SERVER "\"DESKTOP-0NUGCCL\"" /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E', 
+		@database_name=N'master', 
+		@flags=0
+GO
+USE [msdb]
+GO
+EXEC msdb.dbo.sp_update_job @job_name=N'Download_data_tmp', 
+		@enabled=1, 
+		@start_step_id=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=2, 
+		@notify_level_page=2, 
+		@delete_level=0, 
+		@description=N'', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'DESKTOP-0NUGCCL\burbo', 
+		@notify_email_operator_name=N'', 
+		@notify_page_operator_name=N''
+GO
+USE [msdb]
+GO
+DECLARE @schedule_id int
+EXEC msdb.dbo.sp_add_jobschedule @job_name=N'Download_data_tmp', @name=N'Nightly_schedule', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=1, 
+		@freq_subday_interval=0, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=1, 
+		@active_start_date=20190831, 
+		@active_end_date=99991231, 
+		@active_start_time=10000, 
+		@active_end_time=235959, @schedule_id = @schedule_id OUTPUT
+select @schedule_id
+GO
